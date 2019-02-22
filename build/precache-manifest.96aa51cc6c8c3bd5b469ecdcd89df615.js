81635c69e727e9f0fd0db5f4fc67a9b6804d9f92
Vitaliy <ivahiv99@gmail.com>
2019-02-22 14:45:05 +0200

    build

---
+++ build/precache-manifest.96aa51cc6c8c3bd5b469ecdcd89df615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14581df077acd602ecd6f763981419b2",
+    "url": "/ToDo_assignment/static/media/bg1.14581df0.jpg"
+  },
+  {
+    "revision": "75c6afbf4667ef93aa9c",
+    "url": "/ToDo_assignment/static/js/runtime~main.75c6afbf.js"
+  },
+  {
+    "revision": "0b3b98b0a06d1d5e89bf",
+    "url": "/ToDo_assignment/static/js/main.0b3b98b0.chunk.js"
+  },
+  {
+    "revision": "5ab93dfc7339e957d1c3",
+    "url": "/ToDo_assignment/static/js/2.5ab93dfc.chunk.js"
+  },
+  {
+    "revision": "0b3b98b0a06d1d5e89bf",
+    "url": "/ToDo_assignment/static/css/main.4f8827c2.chunk.css"
+  },
+  {
+    "revision": "c9a8826a7ff02377c991714dd3b7aac7",
+    "url": "/ToDo_assignment/index.html"
+  }
+];
